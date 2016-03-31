abd68f7a2ea199a6adf748df98919033a2340083
Alex Tran Qui <alex@earmark.io>
2016-03-31 23:52:10 +0200

    Updated based on PR comments.

--- Sources/Provider.swift
+++ Sources/Provider.swift
@@ -2,10 +2,27 @@ import Stencil
 import PathKit
 import Vapor
 
+extension Application {
+  var resourcesDir: String { return workDir + "Resources/" }
+  var viewsDir: String { return resourcesDir + "Views/" }
+}
+
 public class Provider: Vapor.Provider {
-    public static func boot(application: Application) {
-        View.renderers[".stencil"] = StencilRenderer( namespace: Namespace(),
-                                                      templateLoader: TemplateLoader(paths: [Path(application.workDir + "Resources/")]))
+  public static var renderer: StencilRenderer?
+  
+  public static func boot(application: Application) {
+    let renderer: StencilRenderer
+    
+    if let r = Provider.renderer {
+      renderer = r
+    } else {
+      renderer = StencilRenderer(
+        namespace: Namespace(),
+        templateLoader: TemplateLoader(paths: [Path(application.viewsDir)])
+      )
     }
     
+    View.renderers[".stencil"] = renderer
+  }
+  
 }
