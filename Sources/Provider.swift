cb138cc82e1a7011e09fed64ded2dee397b7de28
Alex Tran Qui <alex@earmark.io>
2016-03-31 16:39:01 +0200

    Updated to work with release 0.4.1 of Vapor and master of Stencil (commit 3c21975) .

--- Sources/Provider.swift
+++ Sources/Provider.swift
@@ -1,10 +1,11 @@
+import Stencil
+import PathKit
 import Vapor
 
 public class Provider: Vapor.Provider {
-	public static let renderer = StencilRenderer()
-
-	public static func boot(application: Application) {
-		View.renderers[".stencil"] = self.renderer
-	}
-
+    public static func boot(application: Application) {
+        View.renderers[".stencil"] = StencilRenderer( namespace: Namespace(),
+                                                      templateLoader: TemplateLoader(paths: [Path(application.workDir + "Resources/")]))
+    }
+    
 }
--- Sources/StencilRenderer.swift
+++ Sources/StencilRenderer.swift
@@ -1,22 +1,21 @@
 import Stencil
-import PathKit
 import Vapor
 
 public class StencilRenderer: RenderDriver {
-	public let namespace: Namespace
-	public let templateLoader: TemplateLoader
-
-	public init(namespace: Namespace = Namespace(), templateLoader: TemplateLoader? = nil) {
-		self.namespace = namespace
-		self.templateLoader = templateLoader ?? TemplateLoader(paths: [Path(View.resourceDir)])
-	}
-
-	public func render(template template: String, context: [String: Any]) throws -> String {
-		let c = Context(dictionary: context)
-		c["loader"] = self.templateLoader
-
-		let template = Template(templateString: template)
-		return try template.render(c, namespace: self.namespace)
-	}
-
+    public let namespace: Namespace
+    public let templateLoader: TemplateLoader
+    
+    public init(namespace: Namespace = Namespace(), templateLoader: TemplateLoader) {
+        self.namespace = namespace
+        self.templateLoader = templateLoader
+    }
+    
+    public func render(template template: String, context: [String: Any]) throws -> String {
+        let c = Context(dictionary: context, namespace: self.namespace)
+        c["loader"] = self.templateLoader
+        
+        let template = Template(templateString: template)
+        return try template.render(c)
+    }
+    
 }
