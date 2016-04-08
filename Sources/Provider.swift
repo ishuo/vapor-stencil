9e3394889855c94117bddf0fe761a4705a43bb48
shnhrrsn <shnhrrsn@gmail.com>
2016-04-08 17:54:00 -0400

    Fixed Provider

--- Sources/Provider.swift
+++ Sources/Provider.swift
@@ -2,23 +2,24 @@ import Stencil
 import PathKit
 import Vapor
 
-
 public class Provider: Vapor.Provider {
-  public var renderer: StencilRenderer?
-  
-  public func boot(application: Application) {
-    let renderer: StencilRenderer
-    
-    if let r = renderer {
-      renderer = r
-    } else {
-      renderer = StencilRenderer(
-        namespace: Namespace(),
-        templateLoader: TemplateLoader(paths: [Path(application.viewsDir)])
-      )
+    public var renderer: StencilRenderer?
+
+    public func boot(application: Application) {
+        let renderer: StencilRenderer
+
+        if let value = self.renderer {
+            renderer = value
+        } else {
+            renderer = StencilRenderer(
+                namespace: Namespace(),
+                templateLoader: TemplateLoader(paths: [Path(application.viewsDir)])
+            )
+
+            self.renderer = renderer
+        }
+
+        View.renderers[".stencil"] = renderer
     }
-    
-    View.renderers[".stencil"] = renderer
-  }
-  
+
 }
